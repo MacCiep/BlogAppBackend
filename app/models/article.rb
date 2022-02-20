fd351e23dda19f6aec8c1c1e5e3b94d2529ab5c0
maciejciepluch <ciepmaciej.dev@gmail.com>
2022-02-20 11:43:42 +0100

    [Articles_testing]-Apply validations to the Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
   include Filterable, HasGenericScopes
-  belongs_to :user
+  belongs_to :user, required: true
   has_many :favourite_articles, dependent: :destroy
+
+  validates :title, :content, presence: true
+  validates :title, length: { minimum: 3, maximum: 50 }
+  validates :content, length: { minimum: 1, maximum: 300 }
 
   generate_default_ilike_scopes :title
 end
